873b5cc1796604685eaabce370532da7e30f4b58
Federico Luzzi <fedeluzzi00@gmail.com>
2022-10-17 20:25:06 +0100

    feat: add fork deploy script

---
+++ fork/deploy.js
@@ -0,0 +1,29 @@
+import ethers from "ethers";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+
+const { ContractFactory } = ethers;
+const require = createRequire(fileURLToPath(import.meta.url));
+
+export const deploy = async (
+    _factory,
+    _kpiTokensManager,
+    _oraclesManager,
+    _multicall,
+    _predictedTemplateId,
+    signer
+) => {
+    const {
+        abi: templateAbi,
+        bytecode: templateBytecode,
+    } = require("../artifacts/OracleTemplate.sol/OracleTemplate.json");
+    const templateFactory = new ContractFactory(
+        templateAbi,
+        templateBytecode,
+        signer
+    );
+    const templateContract = await templateFactory.deploy();
+    await templateContract.deployed();
+
+    return { templateContract };
+};
