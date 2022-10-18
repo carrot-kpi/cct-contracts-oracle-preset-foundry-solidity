1ab4b14c9e30830bd1c2737c91f57f1920887894
Federico Luzzi <fedeluzzi00@gmail.com>
2022-10-18 16:32:04 +0100

    fix: es6 init script

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
-const { execSync } = require("child_process");
+import { execSync } from "child_process";
 
-execSync("forge install carrot=carrot-kpi/contracts-v1 --no-commit")
-execSync("forge install foundry-rs/forge-std --no-commit")
-execSync("forge install dapphub/ds-test --no-commit")
+execSync("forge install carrot=carrot-kpi/contracts-v1 --no-commit");
+execSync("forge install foundry-rs/forge-std --no-commit");
+execSync("forge install dapphub/ds-test --no-commit");
 
-console.log("Foundry dependencies installed")
+console.log("Foundry dependencies installed");
